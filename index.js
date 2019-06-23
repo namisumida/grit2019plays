8f4284a09e0dbd69ecfc4a5f2473974709f128b4
namisumida <sumida.nami@gmail.com>
2019-06-23 10:24:39 -0400

    Adding grime

--- index.js
+++ index.js
@@ -21,19 +21,31 @@ function init() {
   // Force
   svg.append("line")
      .attr("id", "force");
+   // Force 2
+   svg.append("line")
+      .attr("id", "defense1");
   //////////////////////////////////////////////////////////////////
   function reset() { // function that hides elements
+    // Hide elements
     svg.selectAll(".offenseDots").style("display", "none");
-    svg.selectAll("#disc").style("display", "none");
-    svg.selectAll("#force").style("display", "none");
+    svg.select("#disc").style("display", "none");
+    svg.select("#force").style("display", "none");
+    svg.select("#defense1").style("display", "none");
     // default button
     d3.selectAll("button").style("background-color", "#DCDCDC").style("color", "navy");
-    d3.select("#button-animate").style("background-color", "red").style("color", "white");
+    d3.select("#button-animate").style("background-color", "red").style("color", "white").style("display", "inline");
+    // Hide forehand and backhand
+    d3.select("#button-forehand").style("display", "none");
+    d3.select("#button-backhand").style("display", "none");
   }; // end reset
-  function display() { // function that displays elements
+  function display(defense1) { // function that displays elements
     svg.selectAll(".offenseDots").style("display", "inline");
     svg.select("#disc").style("display", "inline");
     svg.select("#force").style("display", "inline");
+    d3.select("#button2-container").style("display", "block");
+    if (defense1==true) {
+      svg.select("#defense1").style("display", "inline");
+    }
   }; // end display
   function set_x() {
     // Players
@@ -65,9 +77,10 @@ function init() {
        .attr("x2", width/2+20)
        .attr("y1", margin.top + 70)
        .attr("y2", margin.top + 55)
-    display();
+    display(false);
   }; // end set_x
   function animate_x() {
+    d3.select("#button-animate").style("display", "none"); // Disable animate
     // under cut
     svg.selectAll(".offenseDots")
        .filter(function(d,i) { return i<6; })
@@ -134,7 +147,10 @@ function init() {
        .attr("cx", width/3)
        .attr("cy", height - margin.top - margin.bottom - 100)
 
-    d3.select("#button-animate").style("display", "none"); // Disable animate
+    d3.select("#button-animate")
+      .transition()
+      .delay(3300)
+      .style("display", "inline");
   }; // end animate_x
   function set_magician() {
     // Players
@@ -163,9 +179,10 @@ function init() {
        .attr("x2", width/2+20)
        .attr("y1", margin.top + 70)
        .attr("y2", margin.top + 55)
-    display();
+    display(false);
   }; // end set_magician
   function animate_magician() {
+    d3.select("#button-animate").style("display", "none"); // Disable animate
     svg.selectAll(".offenseDots")
        .transition()
        .duration(1500)
@@ -214,7 +231,10 @@ function init() {
         .duration(1500)
         .attr("cx", width/2 - 40)
         .attr("cy", height*1.5);
-    d3.select("#button-animate").style("display", "none"); // Disable animate
+    d3.select("#button-animate")
+      .transition()
+      .delay(4000)
+      .style("display", "inline");
   }; // end animate_magician
   function set_zigzag(force) {
     // Players
@@ -255,9 +275,10 @@ function init() {
          .attr("y1", margin.top + 70)
          .attr("y2", margin.top + 55)
     }
-    display();
+    display(false);
   }; // end set_zigzag
   function animate_zigzag_forehand() {
+    d3.select("#button-animate").style("display", "none"); // Disable animate
     svg.selectAll(".offenseDots")
        .transition()
        .duration(1500)
@@ -296,11 +317,15 @@ function init() {
        .transition()
        .delay(2500)
        .duration(500)
-       .attr("cx", width/3 - 20)
+       .attr("cx", width/2 + 50)
        .attr("cy", margin.top + 200);
-    d3.select("#button-animate").style("display", "none"); // Disable animate
+    d3.select("#button-animate")
+      .transition()
+      .delay(3500)
+      .style("display", "inline");
   }; // end animate_zigzag
   function animate_zigzag_backhand() {
+    d3.select("#button-animate").style("display", "none"); // Disable animate
     svg.selectAll(".offenseDots")
        .transition()
        .duration(1500)
@@ -341,7 +366,10 @@ function init() {
        .duration(500)
        .attr("cx", width/2 + 50)
        .attr("cy", margin.top + 200);
-    d3.select("#button-animate").style("display", "none"); // Disable animate
+    d3.select("#button-animate")
+      .transition()
+      .delay(3500)
+      .style("display", "inline");
   }; // end animate_zigzag
   function set_flood() {
     // Players
@@ -370,9 +398,10 @@ function init() {
        .attr("x2", width/2+3)
        .attr("y1", margin.top + 55)
        .attr("y2", margin.top + 70)
-    display();
+    display(false);
   }; // end set_flood
   function animate_flood() {
+    d3.select("#button-animate").style("display", "none"); // Disable animate
     // flood
     svg.selectAll(".offenseDots")
        .filter(function(d,i) { return i<6; })
@@ -404,63 +433,223 @@ function init() {
        .duration(800)
        .attr("cx", width*.8)
        .attr("cy", margin.top + 200);
-    d3.select("#button-animate").style("display", "none"); // Disable animate
+   d3.select("#button-animate")
+     .transition()
+     .delay(2100)
+     .style("display", "inline");
   }; // end animate_flood
+  function set_grime1() {
+    // Players
+    svg.selectAll(".offenseDots")
+       .attr("cx", function(d,i) { // set up
+         if (i==0) { return width/2; } // handler 1
+         else if (i==1) { return width/3; } // handler 2
+         else { return width/2-30; }
+       })
+       .attr("cy", function(d,i) { // set up
+         if (i==0) { return margin.top + 50; } // handler 1
+         else if (i==1) { return margin.top; } // reset 1
+         else if (i==2) { return margin.top + 120; } // top of stack
+         else if (i==3) { return margin.top + 160; } // cutter 1
+         else if (i==4) { return margin.top + 200; } // cutter 2
+         else if (i==5) { return margin.top + 240; } // cutter 3
+         else if (i==6) { return margin.top + 280; } // cutter 4
+       })
+    // Disc
+    svg.select("#disc")
+       .attr("cx", width/2)
+       .attr("cy", margin.top + 50)
+    // Force
+    svg.select("#force")
+       .attr("x1", width/2 - 3)
+       .attr("x2", width/2+20)
+       .attr("y1", margin.top + 70)
+       .attr("y2", margin.top + 55);
+    svg.select("#defense1")
+       .attr("x1", width/2 - 50)
+       .attr("x2", width/2 - 50)
+       .attr("y1", margin.top + 110)
+       .attr("y2", margin.top + 135);
+    display(true);
+  }; // end set_grime1
+  function animate_grime1() {
+    d3.select("#button-animate").style("display", "none"); // Disable animate
+    svg.selectAll(".offenseDots")
+       .filter(function(d,i) { return i==2; })
+       .transition()
+       .duration(1500)
+       .attr("cx", width/2+20);
+    svg.select("#disc")
+       .transition()
+       .duration(1500)
+       .attr("cx", width/2+20)
+       .attr("cy", margin.top + 120);
+    svg.select("#defense1")
+       .transition()
+       .duration(3500)
+       .attr("x1", width/2+15)
+       .attr("x2", width/2+35)
+       .attr("y1", margin.top + 145)
+       .attr("y2", margin.top + 145);
+    svg.selectAll(".offenseDots")
+       .filter(function(d,i) { return i==6; })
+       .transition()
+       .delay(1500)
+       .duration(1500)
+       .attr("cx", width/2)
+       .attr("cy", height*1.5);
+    svg.select("#disc")
+       .transition()
+       .delay(1500)
+       .duration(1500)
+       .attr("cx", width/2)
+       .attr("cy", height*1.5);
+    d3.select("#button-animate")
+      .transition()
+      .delay(3500)
+      .style("display", "inline");
+  }; // end animate_grime1
+  function set_grime2() {
+    // Players
+    svg.selectAll(".offenseDots")
+       .attr("cx", function(d,i) { // set up
+         if (i==0) { return width/2; } // handler 1
+         else if (i==1) { return width/3; } // handler 2
+         else { return width/2-30; }
+       })
+       .attr("cy", function(d,i) { // set up
+         if (i==0) { return margin.top + 50; } // handler 1
+         else if (i==1) { return margin.top; } // reset 1
+         else if (i==2) { return margin.top + 120; } // top of stack
+         else if (i==3) { return margin.top + 160; } // cutter 1
+         else if (i==4) { return margin.top + 200; } // cutter 2
+         else if (i==5) { return margin.top + 240; } // cutter 3
+         else if (i==6) { return margin.top + 280; } // cutter 4
+       })
+    // Disc
+    svg.select("#disc")
+       .attr("cx", width/2)
+       .attr("cy", margin.top + 50)
+    // Force
+    svg.select("#force")
+       .attr("x1", width/2 - 3)
+       .attr("x2", width/2+20)
+       .attr("y1", margin.top + 70)
+       .attr("y2", margin.top + 55);
+    svg.select("#defense1")
+       .attr("x1", width/2 - 45)
+       .attr("x2", width/2 - 22)
+       .attr("y1", margin.top + 105)
+       .attr("y2", margin.top + 100);
+    display(true);
+  }; // end set_grime2
+  function animate_grime2() {
+    d3.select("#button-animate").style("display", "none"); // Disable animate
+    svg.selectAll(".offenseDots")
+       .filter(function(d,i) { return i==3; })
+       .transition()
+       .duration(1500)
+       .attr("cx", width/2+20);
+    svg.select("#disc")
+       .transition()
+       .duration(1500)
+       .attr("cx", width/2+20)
+       .attr("cy", margin.top + 160);
+    svg.selectAll(".offenseDots")
+       .filter(function(d,i) { return i==6; })
+       .transition()
+       .delay(1500)
+       .duration(1500)
+       .attr("cx", width/2)
+       .attr("cy", height*1.5);
+    svg.select("#disc")
+       .transition()
+       .delay(1500)
+       .duration(1500)
+       .attr("cx", width/2)
+       .attr("cy", height*1.5);
+    d3.select("#button-animate")
+      .transition()
+      .delay(3500)
+      .style("display", "inline");
+  }; // end animate_grime2
+  function buttonClicked(button) {
+    button.style("background-color", "navy");
+    button.style("color", "white");
+  }; // end buttonClicked
+  function buttonUnclicked(button) {
+    button.style("background-color", d3.color("#DCDCDC"));
+    button.style("color", "navy");
+  }; // end buttonUnclicked
   d3.select("#button-x").on("click", function() {
     reset();
     set_x();
-    // button style
-    d3.select(this).style("background-color", "navy");
-    d3.select(this).style("color", "white");
-    d3.select("#button-animate").style("display", "inline");
+    buttonClicked(d3.select(this));
     d3.select("#button-animate").on("click", function() {
       animate_x();
+      set_x();
     })
   });
   d3.select("#button-flood").on("click", function() {
     reset();
     set_flood();
-    // button style
-    d3.select(this).style("background-color", "navy");
-    d3.select(this).style("color", "white");
-    d3.select("#button-animate").style("display", "inline");
+    buttonClicked(d3.select(this));
     d3.select("#button-animate").on("click", function() {
       animate_flood();
+      set_flood();
     })
   });
   d3.select("#button-magician").on("click", function() {
     reset();
     set_magician();
-    // button style
-    d3.select(this).style("background-color", "navy");
-    d3.select(this).style("color", "white");
-    d3.select("#button-animate").style("display", "inline");
+    buttonClicked(d3.select(this));
     d3.select("#button-animate").on("click", function() {
       animate_magician();
+      set_magician();
     })
   });
-  d3.select("#button-zigzag-f").on("click", function() {
+  d3.select("#button-zigzag-forehand").on("click", function() {
     reset();
+    buttonClicked(d3.select(this));
+    buttonClicked(d3.select("#button-zigzag"));
+    buttonUnclicked(d3.select("#button-backhand"));
     set_zigzag("forehand");
-    // button style
-    d3.select(this).style("background-color", "navy");
-    d3.select(this).style("color", "white");
-    d3.select("#button-animate").style("display", "inline");
     d3.select("#button-animate").on("click", function() {
       animate_zigzag_forehand();
+      set_zigzag("forehand");
     })
   });
-  d3.select("#button-zigzag-b").on("click", function() {
+  d3.select("#button-zigzag-backhand").on("click", function() {
     reset();
+    buttonClicked(d3.select(this));
+    buttonClicked(d3.select("#button-zigzag"));
+    buttonUnclicked(d3.select("#button-backhand"));
     set_zigzag("backhand");
-    // button style
-    d3.select(this).style("background-color", "navy");
-    d3.select(this).style("color", "white");
-    d3.select("#button-animate").style("display", "inline");
     d3.select("#button-animate").on("click", function() {
       animate_zigzag_backhand();
+      set_zigzag("backhand");
+    })
+  })
+  d3.select("#button-grime1").on("click", function() {
+    reset();
+    set_grime1();
+    buttonClicked(d3.select(this));
+    buttonClicked(d3.select("#button-grime"));
+    d3.select("#button-animate").on("click", function() {
+      animate_grime1();
+      set_grime1();
     })
   });
+  d3.select("#button-grime2").on("click", function() {
+    reset();
+    set_grime2();
+    buttonClicked(d3.select(this));
+    buttonClicked(d3.select("#button-grime"));
+    d3.select("#button-animate").on("click", function() {
+      animate_grime2();
+      set_grime2();
+    })
+  })
 };
 
 var numEntered = []; // array to hold numbers entered
@@ -484,3 +673,9 @@ d3.select("#button-enter").on("click", function() {
     numEntered = [];
   }
 })
+
+/*
+// for testing
+d3.select("#main").style("display", "inline");
+init();
+*/
